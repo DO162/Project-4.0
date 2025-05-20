049db332e396bc71400b9c585a4d1fa510b67b03
DO162 <ds9893814@gmail.com>
2025-05-20 16:08:54 +0300

    Add files via upload
    Пример для музыки

---
+++ test/test.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+#pragma comment(lib, "winmm.lib")
+
+DWORD WINAPI MusicThread(void* param)
+{
+	PlaySoundA("C:\\Users\\sdk26\\Downloads\\city-bgm-336601.WAV", NULL, SND_NODEFAULT | SND_LOOP);
+	return 0;
+}
+
+void Action()
+{
+	while (true)
+	{
+		cout << ".";
+		Sleep(1000);
+	}
+}
+
+int main()
+{
+	system("title PlaySound Demo");
+	CreateThread(0, 0, MusicThread, 0, 0, 0);
+	Action();
+}
